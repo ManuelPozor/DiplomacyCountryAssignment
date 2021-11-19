07b0731d335f8bd145cc9bf0a3cbc8bdecacc1f3
Manuel Pozor <manuel.pozor@rwth-aachen.de>
2021-11-19 16:46:46 +0100

    dropdown functionality storing selection as json

--- server.py
+++ server.py
@@ -7,6 +7,13 @@ import uuid
 app = Flask(__name__)
 players_file = Path("player_priorities.json")
 
+unique_country_tags = ["GB", "FR", "GE", "IT", "AH", "RU", "OE"]
+country_names = [
+    "Great Britain", "France", "German Empire", "Italy", "Austria-Hungary",
+    "Russia", "Ottoman Empire"
+]
+
+
 def get_player_by_id(id):
     with open(players_file, "r") as file:
         players = [player for player in json.load(file)]
@@ -19,15 +26,69 @@ def get_player_by_id(id):
     return player_data
 
 
+@app.route('/result/<id>')
+def result(id):
+    return 'welcome %s' % id
+
+
+@app.route("/<id>")
+def start(id):
+    # check if player id correct
+    player = get_player_by_id(id)
+    if player is None:
+        return 'ERROR: Unknown player'
+    # load priorities
+    priorities = [player["prio1"], player["prio2"], player["prio3"]]
+    already_submitted = player["submitted"]
+    # TODO if assignment over redirect straight to result
+    return render_template("country_selection.html",
+                           id=id,
+                           tags=unique_country_tags,
+                           country_names=country_names,
+                           priorities=priorities,
+                           submitted=already_submitted)
+
+
 @app.route("/")
 def home():
-    return render_template("country_selection.html")
+    return "Please use your unique access link."
 
+
+@app.route('/search', methods=['GET'])
+def priorities_submitted():
+    prio1 = request.args.get('prio1')
+    prio2 = request.args.get('prio2')
+    prio3 = request.args.get('prio3')
+    id = request.args.get('id')
+
+    with open(players_file, "r") as file:
+        players = [player for player in json.load(file)]
+    # set status to submitted
+    for p in players:
+        if p["id"] == id:
+            p["submitted"] = True
+            p["prio1"] = prio1
+            p["prio2"] = prio2
+            p["prio3"] = prio3
+    # save player in json
+    with open(players_file, "w") as file:
+        json.dump(players, file, indent=4, sort_keys=True)
+
+    # check if all players have submitted
+    for p in players:
+        if not p["submitted"]:
+            # TODO throws error when submitting again
+            return redirect(url_for('start', id=id))
+
+    # TODO assignment
+    return redirect(url_for('result'), id=id)
 
 
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser(description='Starts a local webserver.')
     # args = parser.parse_args()
+
+    # TODO arg to generate new ids
 
     # create player ids in json
     with open(players_file, "r") as file:
